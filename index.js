31e5d6db0c89c4853dab98272c9a9c3736add491
EcheCostabel <exequielcostabel@gmail.com>
2023-01-24 20:15:53 -0300

    cleaning js

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ let abrir = document.getElementById('abrir');
 let cerrar = document.getElementById('close');
 
 
+
 abrir.addEventListener('click', function(){
     modal.style.display = 'block'
-})
+});
 
 cerrar.addEventListener('click', function(){
     modal.style.display = 'none'
-})
+});
 
 window.addEventListener('click', function(e){
     if(e.target === flex){
         modal.style.display = 'none'
-    }
-})
+    };
+});
